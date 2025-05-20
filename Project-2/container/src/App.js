de45c482e1a483c75d51d47032aabfeab2c0c142
Krishnadev <krishandev.thandiyekkalradhakr@dcmail.ca>
2025-05-20 13:23:04 -0400

    Fixed css issue

--- Project-2/container/src/App.js
+++ Project-2/container/src/App.js
@@ -1,15 +1,21 @@
-import React from  "react"
+import React from "react"
 import ReactDOM from 'react-dom';
+import { StylesProvider, createGenerateClassName } from "@material-ui/core/styles";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co',
+});
 export const App = () => {
     return (
         <BrowserRouter>
-            <div>
-                <Header />
-                <MarketingApp />
-            </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
--- Project-2/marketing/src/ReactApp.js
+++ Project-2/marketing/src/ReactApp.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
 
-import Landing from '../components/Landing.js';
-import Pricing from '../components/Pricing.js';
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
 
-
-export const ReactApp =  () => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route path="/" component={Landing} />
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
+export default () => {
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
     </div>
-}
+  );
+};
